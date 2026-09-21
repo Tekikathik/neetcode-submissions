6663f98f6a7d8a9fca050e73556fdf30200ed714
teki karthik <147475789+Tekikathik@users.noreply.github.com>
2026-09-21 22:38:43 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,23 @@
+class MinStack {
+public:
+    stack<int> st;
+    stack<int> minst;
+    MinStack() {
+    }
+    void push(int val) {
+        if (minst.empty() || val<=minst.top()){
+            minst.push(val);
+        }
+        st.push(val);
+    }
+    void pop() {
+        if (st.top()==minst.top()) minst.pop();
+        st.pop();   
+    }
+    int top() {
+        return st.top();   
+    }
+    int getMin() {
+        return minst.top();
+    }
+};
